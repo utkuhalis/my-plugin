626fb4729c65dac02150c8a43271f88e8aa297fc
Utku Halis <utkuhalis97@gmail.com>
2023-06-04 04:50:43 +0300

    .

---
+++ src/Migrations/20230604120000_create_example_table.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace YourNamespace\MyPlugin\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateExampleTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'null' => false
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => false
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('example');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('example');
+    }
+}
--- src/MyPlugin.php
+++ src/MyPlugin.php
@@ -2,10 +2,19 @@
 
 namespace YourNamespace\MyPlugin;
 
+use YourNamespace\MyPlugin\Database\Migrations\CreateExampleTable;
+
 class MyPlugin
 {
     public function hello()
     {
         return 'Merhaba, benim eklentim!';
+    }
+
+    public function install()
+    {
+        $this->migrationsNamespace = 'YourNamespace\\MyPlugin\\Database\\Migrations';
+        parent::install();
+        $this->migrate(new CreateExampleTable());
     }
 }
